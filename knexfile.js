50dc607bb3d9ae0f1e9f395ecb0ee54c8599312c
Summer Barclay <evil_lyns@yahoo.com>
2017-04-22 12:05:38 -0700

    Debugging commit 2

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,24 @@
 const path = require('path')
 
 module.exports = {
-    development: {
-        client: 'pg',
-        connection: 'postgres://localhost/tramp_walkers_dev',
-        migrations: {
-            directory: path.join(__dirname, 'db', 'migrations')
-        },
-        seeds: {
-            directory: path.join(__dirname, 'db', 'seeds')
-        }
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/tramp_walkers_dev',
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
     },
-    production: {
-        client: 'pg',
-        connection: process.env.DATABASE_URL,
-        migrations: {
-            directory: path.join(__dirname, 'db', 'migrations')
-        },
-        seeds: {
-            directory: path.join(__dirname, 'db', 'seeds')
-        }
+      seeds: {
+        directory: path.join(__dirname, 'db', 'seeds')
     }
+  },
+  production: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    directory: path.join(__dirname, 'db', 'migrations')
+  },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  }
 }
